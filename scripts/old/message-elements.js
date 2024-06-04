2e3a3957fe837329a2deff9411239cad18a55989
bcushenberry <cushenberry@gmail.com>
2024-06-04 11:56:49 +0900

    Removed some initial workups that are no longer needed

--- scripts/old/message-elements.js
+++
@@ -1,3 +0,0 @@
-const messageElement = document.getElementById("message");
-
-export { messageElement };
--- scripts/old/reset.js
+++
@@ -1,8 +0,0 @@
-const armorListElement = document.getElementById("armorList");
-export const messageElement = document.getElementById("message");
-
-/* Create a function that resets the armor list and message */
-export const reset = function () {
-    armorListElement.innerHTML = "";
-    messageElement.textContent = "";
-};
