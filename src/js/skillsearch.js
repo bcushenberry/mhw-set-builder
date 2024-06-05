9870f4051dc1862094c7b206e2ebbfd215c2e6e6
bcushenberry <cushenberry@gmail.com>
2024-06-05 16:34:50 +0900

    Better formatted

--- src/js/skillsearch.js
+++ src/js/skillsearch.js
@@ -1,8 +1,13 @@
 import { getSkills } from "./SkillList.mjs";
-import { getArmorList, displayMatchingArmor, armorList, rankElement } from "./ArmorList.mjs";
+import {
+  getArmorList,
+  displayMatchingArmor,
+  armorList,
+  rankElement,
+} from "./ArmorList.mjs";
 
 document.getElementById("skillsList").addEventListener("change", () => {
-    displayMatchingArmor(armorList);
+  displayMatchingArmor(armorList);
 });
 
 getSkills();
